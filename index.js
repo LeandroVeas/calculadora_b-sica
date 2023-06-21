278840257a81b58da279940fc6899bfe39b7c17e
LeandroVeas <93226973+LeandroVeas@users.noreply.github.com>
2023-06-20 21:30:10 -0300

    corregido

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button")
+//Declaración constantes
+const display = document.getElementById("display");
+const teclas = document.querySelectorAll("button")
 
-buttons.forEach((btn)=>{
-    btn.addEventListener("click",()=>{
-        if (btn.id === "="){
-            display.value = eval(display.value)
-        }else if (btn.id === "ac"){
-            display.value = ""
+//Se usa un forEach para recorrer los botones.
+// y cada vez que se oprime uno llama a un evento
+teclas.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        if(btn.id == "="){
+            display.value = eval(display.value);// eval, evaluá lo que esta dentro
+        }else if (btn.id == "ac"){
+            display.value = ""; // devuelve un string vacio
         }else if (btn.id == "de"){
-            display.value = display.value.slice(0,-1)
-        }else {
-            display.value +=btn.id
+            display.value = display.value.slice(0,-1)// Se usa slice para ur sacando el ultimo elemento
+        }else{
+            display.value += btn.id
         }
     })
 })
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
